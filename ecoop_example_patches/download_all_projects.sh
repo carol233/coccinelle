e50d96dfcfe990367a633cd30863a1a0a59c09f2
Kang Hong Jin <kanghongjin@gmail.com>
2019-04-22 14:16:46 +0800

    remove project used for negative example

--- ecoop_example_patches/download_all_projects.sh
+++ ecoop_example_patches/download_all_projects.sh
@@ -1,7 +1,6 @@
 cd get_color && ./download_project.sh && cd ..
 cd get_height && ./download_project.sh && cd ..
 cd should_vibrate && ./download_project.sh && cd ..
-cd get_drawable && ./download_project.sh && cd ..
 cd on_console_message && ./download_project.sh && cd ..
 cd set_text_size && ./download_project.sh && cd ..
 cd sticky_broadcasts && ./download_project.sh && cd ..
