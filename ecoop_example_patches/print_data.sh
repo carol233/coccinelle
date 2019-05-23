1e427c57d3d89ef449f999f6cb3f02dc4e8e8eba
Kang Hong Jin <kanghongjin@gmail.com>
2019-05-23 14:42:25 +0800

    add echos to script

--- ecoop_example_patches/print_data.sh
+++ ecoop_example_patches/print_data.sh
@@ -1,3 +1,6 @@
+echo "Number of files"
 grep -ce "diff" ./*/project.patch
-grep -cve "^\s*$" ./*/*.cocci
+echo "Number of additions and deletions"
 grep -ce "^[-+]\s" ./*/project.patch
+echo "Size of Semantic Patches"
+grep -cve "^\s*$" ./*/*.cocci
